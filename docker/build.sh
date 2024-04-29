cffadb6ea9d5302c4ac50bd2e6cf5a205070a04b
Diljot Grewal <diljot.grewal@gmail.com>
2024-04-29 12:46:13 -0700

    testing

--- docker/build.sh
+++ docker/build.sh
@@ -4,26 +4,15 @@ set -e
 TYPE=$1
 USERNAME=$2
 PASSWORD=$3
-BETA=$4
 
 VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 
-if [ $BETA == "Y" ]
-then 
-    DOCKER_VERSION="${VERSION}beta"
-else
-    DOCKER_VERSION=$VERSION
-fi
-
 cd $TYPE
 
-
-docker build --build-arg VERSION=$VERSION -t quay.io/mondrianscwgs/$TYPE:$DOCKER_VERSION .
-
-
+docker build --build-arg VERSION=$VERSION -t quay.io/mondrianscwgs/$TYPE:$VERSION .
 
 docker login quay.io -u $USERNAME --password $PASSWORD
-docker push quay.io/mondrianscwgs/$TYPE:$DOCKER_VERSION
+docker push quay.io/mondrianscwgs/$TYPE:$VERSION
 
 
 cd ../
