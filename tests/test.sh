11172a139594692fdac3559a6e2c1e4a442c67a4
Diljot Grewal <diljot.grewal@gmail.com>
2024-04-29 11:25:09 -0700

    testing

--- tests/test.sh
+++ tests/test.sh
@@ -11,19 +11,15 @@ TEMP_DIR=$TEST_DIR/$PIPELINE
 mkdir -p $TEMP_DIR && cd $TEMP_DIR
 
 wget -nv https://mondriantestdata.s3.amazonaws.com/${TEST_DATA}_testdata.tar.gz
-tar -xvf ${TEST_DATA}_testdata.tar.gz
+tar -xvf ${TEST_DATA}_testdata.tar.gz -C data
 
-
+DATA_DIR=`ls -d $PWD/data/*`
 
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
-sed -i 's@docker_image_here@quay.io/mondrianscwgs/'"$DOCKER"':'"$TAG"beta'@g' ${TEST_DIR}/${PIPELINE}.json
-sed -i 's@mondrian-ref-path-here@'"$RESOURCE_DIR"'/mondrian-ref-20-22@g' ${TEST_DIR}/${PIPELINE}.json
+sed -i 's@mondrian-ref-path-here@'"$RESOURCE_DIR"'/mondrian-ref-20-22@g' ${TEST_DIR}/${PIPELINE}.yaml
+sed -i 's@mondrian-data-path-here@'"$DATA_DIR"'/@g' ${TEST_DIR}/${PIPELINE}.yaml
+sed -i 's@mondrian-data-path-here@'"$DATA_DIR"'/@g' ${TEST_DIR}/$samplesheet.csv
 
-
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$"
-cat ${TEST_DIR}/${PIPELINE}.json
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$"
-
-java -Dconfig.file=run.config -jar $RESOURCE_DIR/cromwell.jar run ${CODEBUILD_SRC_DIR}/mondrian/${PIPELINE}.wdl -i ${TEST_DIR}/${PIPELINE}.json -o options.json --imports $RESOURCE_DIR/imports_${TAG}.zip
-
+$RESOURCE_DIR/nextflow pull -r $TAG
+$RESOURCE_DIR/nextflow run -r $TAG -params-file params.yaml -resume --max_cpus 4  -profile docker
 
